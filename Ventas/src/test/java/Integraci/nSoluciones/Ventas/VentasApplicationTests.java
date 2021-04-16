ac1dd8d960ad874c776e9803c563938363bf84f3
Luzmarisol1 <zs17015187@estudiantes.uv.mx>
2021-04-15 22:29:41 -0500

    ventas

---
+++ Ventas/src/test/java/Integraci/nSoluciones/Ventas/VentasApplicationTests.java
@@ -0,0 +1,13 @@
+package Integraci.nSoluciones.Ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
